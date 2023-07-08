3650075b8abecfd5481ff836a530191b307de5af
RomarTests <138975394+RomarTests@users.noreply.github.com>
2023-07-08 11:50:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo!')
+    res.send('Yo! C\'est les finances Poulet  qui vous parle.')
 })
 app.listen(process.env.PORT || 3000)
